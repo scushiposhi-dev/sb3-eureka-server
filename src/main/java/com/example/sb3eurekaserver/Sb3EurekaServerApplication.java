9bed4f609c3d44978e2d78d162dd60f68f8d5b43
scushiposhi <scushiposhi@yahoo.com>
2023-12-15 17:13:51 +0100

    docker-compose Dockerfile

---
+++ src/main/java/com/example/sb3eurekaserver/Sb3EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.sb3eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Sb3EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3EurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb3eurekaserver/Sb3EurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sb3eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class Sb3EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertNotNull(Sb3EurekaServerApplicationTests.class);
+    }
+
+}
